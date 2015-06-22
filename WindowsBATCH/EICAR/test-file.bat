b0601f9859001a0460b43e31e28b853d9d9bae5a
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2015-06-22 11:33:43 +0530

    add eicar test file.bat
    a batch script to test if the antivirus engine is working

---
+++ WindowsBATCH/EICAR/test-file.bat
@@ -0,0 +1,9 @@
+@echo off
+title :: EICAR ::
+cls
+REM todo add readable content
+:exec
+echo "X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*" > C:\eicar.com
+echo "file created"
+REM todo add readable content
+
