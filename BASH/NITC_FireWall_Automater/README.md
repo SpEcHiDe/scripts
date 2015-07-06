f6b5659bc28b306c44108290b5b55e385344137c
Shrimadhav U K <magicteacher95@gmail.com>
2015-07-06 09:28:16 +0530

    documented the fortinet firewall code

--- BASH/NITC_FireWall_Automater/README.md
+++ BASH/NITC_FireWall_Automater/README.md
@@ -2,7 +2,4 @@
 
 a bash script to automate login to firewall when there is no GUI browser
 
-#### TO DO
-- [ ] document the code properly
-- [ ] improve the code design
 
--- BASH/NITC_FireWall_Automater/script.sh
+++ BASH/NITC_FireWall_Automater/script.sh
@@ -4,18 +4,24 @@ if [ $# -ne 1 ]; then
     echo "usage instructions : sh ${0} <login-user-name> <login-password>"
 else
 
-host=173.194.36.0   # give some ip address
-port=23
-# copied from http://www.linuxquestions.org/questions/programming-9/shell-script-to-automate-telnet-573950/
+	host=173.194.36.0   # give some ip address
+	port=23
+	# copied from http://www.linuxquestions.org/questions/programming-9/shell-script-to-automate-telnet-573950/
 
-( echo open ${host} ${port}
-  sleep 1
-  echo ${1}
-  echo -e "\r"
-  sleep 1
-  echo ${2}
-  sleep 1
-  echo -e "\r"
-  echo exit ) | telnet
+	# the commands in brackets are the telnet commands
+	( 
+	  echo open ${host} ${port} # telnet> open 173.194.36.0 23
+	  sleep 1					
+	  # Welcome to Fortinet Firewall prompt will appear
+	  # a login prompt will wait for user input
+	  echo ${1}					# type in the user name at the login prompt
+	  echo -e "\r"				# press "<--| enter" after typing the user name
+	  sleep 1
+	  # a password prompt will appear waiting for user input
+	  echo ${2}					# type in the password at the prompt
+	  echo -e "\r"				# press "<--| enter" after typing the password
+	  sleep 1
+	  echo exit 				# exit from the telnet client
+	) | telnet
   
 fi
