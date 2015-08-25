fb80fd8d1ae6352fe19df852e0e6b9f010fb8eaf
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2015-08-25 20:01:40 +0530

    firewall automation script for NITC students

---
+++ Python/NITC_FireWall_Automater/script.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2 -tt
+
+import getpass
+import sys
+import telnetlib
+import os
+
+def firewall_login(username,password,host) :
+	tn = telnetlib.Telnet(host)
+	tn.read_until("login: ")
+	tn.write(username + "\n")
+	if password:
+		tn.read_until("password: ")
+		tn.write(password + "\n")
+	tn.write("logout\n")
+	tn.write("exit\n")
+	print tn.read_all()
+
+def usage() :
+	print "RTFM"
+
+def main() :
+	host = str("www.google.com")
+	username = str(sys.argv[1])
+	password = str(sys.argv[2])
+	count = 0
+	while True :
+		try :
+			print "Press Ctrl C to quit"
+			isnetacs = os.system("ping -c1 "+host+" > /dev/null 2>&1")
+			if isnetacs != 0 :
+				firewall_login(username,password,host)
+				count = count + 1
+			  print "Login "+str(count)+" time(s)"
+		except KeyboardInterrupt :
+			sys.exit(-1)
+
+if __name__ == "__main__" :
+	if len(sys.argv) == 3 :
+		main()
+	else :
+		usage()
