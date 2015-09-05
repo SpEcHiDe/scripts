aa900e60e39ae1dbe230b78e0597d930ee98c57f
Shrimadhav U K <magicteacher95@gmail.com>
2015-09-05 20:46:50 +0530

    started fb verruppikals directory

---
+++ Javascript/FB verruppikkals/invite_to_like.js
@@ -0,0 +1,11 @@
+
+var inputs = document.getElementsByClassName('_1sm'); 
+// class name of the invite button
+
+for(var i=0;i<inputs.length;i++) { 
+
+    // click on the focused element
+    inputs[i].click(); 
+    
+
+}
---
+++ Javascript/FB verruppikkals/send_pm_to_all_online.js
@@ -0,0 +1,13 @@
+
+var inputs = document.getElementsByClassName('_55ln'); 
+// class name of the chat sidebar
+
+for(var i=0;i<inputs.length;i++) { 
+
+    // click on the focused element
+    inputs[i].click(); 
+    
+    // get focus to the textfield by ClassName 'uiTextareaAutogrow _552m'
+
+    // and click on the current 'close button'
+}
