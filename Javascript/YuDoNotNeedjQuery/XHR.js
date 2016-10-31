e4d54a46ea04468385016928ca43cca0cb5da7bb
Shrimadhav U K <magicteacher95@gmail.com>
2016-10-31 08:25:08 +0530

    add XHR.js

---
+++ Javascript/YuDoNotNeedjQuery/XHR.js
@@ -0,0 +1,25 @@
+var RequestData = function(type, URL, formData, callBack){
+  // create a XHR object
+  var xhr = new XMLHttpRequest();
+  // open the XHR object in asynchronous mode
+  xhr.open(type, URL, true);
+  if(type == "POST"){
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded; charset=ISO-8859-1');
+  }
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      // OK! we have a successful response.
+      var response = xhr.responseText;
+      //console.log('OUTPUT: ' + response);
+      // do something else with the response
+      callBack(URL, response);
+    }
+  };
+  // GET or POST the URL according to type
+  if(type == "GET"){
+    xhr.send();
+  }
+  if(type == "POST"){
+    xhr.send(formData);
+  }
+};
