6911687dd5d726cc434228d22895ba9b9156a21a
Shrimadhav U K <shrimadhavuk@gmail.com>
2017-01-12 19:02:03 +0530

    Android Shell Screenshot

--- BASH/Android/RemoteScreenShot.sh
+++ BASH/Android/RemoteScreenShot.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-## RemoteScreenShot is a shell script which aims to help take screenshots 
+## RemoteScreenShot is a shell script which aims to help take screenshots
 ## without having to press some complicated buttons simultaneously!
 
-## No Validations Done! 
+## No Validations Done!
 ## TODO: the validations
 
 function take_screenshot(){
   # usage: take_screenshot output.png
-  adb shell screencap -p | sed 's/\r$//' > ${1}
+  adb shell screencap -p | sed 's/\r\n$//' > ${1}
 }
 
 function main(){
   echo "press 0 at any time to exit"
   echo "connect your Android device to the computer with USB debugging enabled!"
+  k="SpEcHiDe" # an unique ID
   j=1 # initialize the variable
   while true; do
+    echo "enter any number dummy variable"
     read dummy_variable
     if [ ${dummy_variable} -eq 0 ]; then
       break;
     else
-      take_screenshot ${j}.png
+      take_screenshot ${k}_${j}.png
+      echo "screenshot saved in pwd"
       j=$(( j + 1 )) # increase variable by 1
     fi
   done;
