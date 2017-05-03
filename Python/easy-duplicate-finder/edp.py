2813977eb09b29a2f82cd17c3ffabad8d372c133
Shrimadhav U K <magicteacher95@gmail.com>
2017-05-03 12:04:44 +0530

    add save to disk and documentation

--- Python/easy-duplicate-finder/edp.py
+++ Python/easy-duplicate-finder/edp.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python
-
+# -*- coding: UTF-8 -*-
 # written by Shrimadhav U K
 
+# perform various file operations like creating and listing directories
 import os
+# obtain the md5 hash of a given file
 import hashlib
+# take command line arguments and proper termination of program
 import sys
+# to save(, or load) the dictionary to disk! (maybe!)
+import json
 
-def unknownostype(name) :
-    print "[-] detected operating system as",name,"\n"
-    print "[-] cannot proceed further. exiting"
-    sys.exit(-1)
-
-def removefile(name) :
-    opt = raw_input("do you want to remove "+name+"(y)")
-    if opt == "y" :
+def removefile(name):
+    opt = str(raw_input("do you want to remove " + name + "(y)"))
+    # opt = "y"
+    if opt == "y":
         os.remove(name)
-        print "[REMOVED]",name
+        print("[REMOVED]", name)
 
 # function taken from thesubdb.com/api/
 def get_hash(name):
@@ -35,30 +36,45 @@ def cni_dict(foldername, dictionary):
         if os.path.isfile(curfile):
             hash = str(get_hash(curfile))
             name = str(curfile)
-            print "[INFORMATION]",hash,":",name
+            # print("[INFORMATION]", hash, ":", name)
             if dictionary.has_key(hash):
                 # filepointer.write("rm -i \""+name+"\"\n")
-                print "[DUPLICATE FOUND] ",name," - ",dictionary[hash]
+                print("[DUPLICATE FOUND] ", name, " - ", dictionary[hash])
                 removefile(name)
             else:
                 dictionary[hash] = name
         else:
-            # print dictionary
-            print "[INFORMATION] cd", curfile
+            # print(dictionary)
+            # print("[INFORMATION] cd", curfile)
             cni_dict(curfile, dictionary)
-            print "[INFORMATION] cd .."
+            # print("[INFORMATION] cd ..")
     return dictionary
 
-def usage() :
-    print "usage instructions : "
-    print "python",sys.argv[0],"<directory name to search>"
+def load_dict_from_file(filename):
+    with open(filename, 'r') as f :
+        dictionary = json.load(f)
+    return dictionary
+
+def save_dict_to_file(dictionary, filename):
+    with open(filename, 'w') as f :
+        json.dump(dictionary, f)
+
+def usage():
+    print("usage instructions : ", "python", sys.argv[0],
+          "<directory name to search>", " [dictionary dump]")
 
 # the main function
 if __name__ == '__main__':
-    if len(sys.argv) != 2 :
+    if len(sys.argv) < 2:
         usage()
-    else :
+    else:
         directoryname = str(sys.argv[1])
-        print "creating the dictionary"
+        filename = "EasyDuplicateFinderbySpEcHiDe.dictionary"
+        print("creating, or loading the dictionary")
+        # TODO: load_dict_from_file 
         dictionary = {}
+        # if len(sys.argv) == 3 :
+        #     filename = sys.argv[2]
+        #     dictionary = load_dict_from_file(filename)
         dictionary = cni_dict(directoryname, dictionary)
+        save_dict_to_file(dictionary, filename)
